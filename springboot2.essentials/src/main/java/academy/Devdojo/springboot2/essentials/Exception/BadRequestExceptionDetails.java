662bb02ad59e3ec30ca34fe1773e53d02a30be96
Erick Caetano <erick.caetano1200@gmail.com>
2023-11-04 23:53:22 -0300

    Handler Global

---
+++ springboot2.essentials/src/main/java/academy/Devdojo/springboot2/essentials/Exception/BadRequestExceptionDetails.java
@@ -0,0 +1,19 @@
+package academy.Devdojo.springboot2.essentials.Exception;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class BadRequestExceptionDetails {
+
+    private  String title;
+    private int status;
+    private  String details;
+    private String developerMessage;
+    private LocalDateTime timestamp;
+
+
+}
